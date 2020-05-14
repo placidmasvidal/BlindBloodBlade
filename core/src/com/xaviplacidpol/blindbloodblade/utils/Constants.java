70d2b576f55a44a57d02c4d6aef26c2e1b1e4807
sarrons <sarronsportatil@gmail.com>
2020-05-14 12:10:22 +0200

    constants modified

--- core/src/com/xaviplacidpol/blindbloodblade/utils/Constants.java
+++ core/src/com/xaviplacidpol/blindbloodblade/utils/Constants.java
@@ -10,8 +10,8 @@ public class Constants {
 
     // World
     public static final Color BACKGROUND_COLOR = Color.SKY;
-    public static final float WORLD_SIZE = 460;
-    public static final float GRAVITY = 100;
+    public static final float WORLD_SIZE = 480;
+    public static final float GRAVITY = 400;
 
     //Kill line when player fall over ground
     public static final float KILL_PLANE = -100;
@@ -25,9 +25,9 @@ public class Constants {
     public static final float PLAYER_EYE_HEIGHT = 16.0f;
     public static final float PLAYER_STANCE_WIDTH = 21.0f;
     public static final float PLAYER_HEAD_HEIGHT = 23.0f;
-    public static final float PLAYER_MOVE_SPEED = 100;
+    public static final float PLAYER_MOVE_SPEED = 200;
 
-    public static final float JUMP_SPEED = 200;
+    public static final float JUMP_SPEED = 300;
     public static final float MAX_JUMP_DURATION = 0.1f;
     public static final float WALK_LOOP_DURATION = 0.25f;
 
