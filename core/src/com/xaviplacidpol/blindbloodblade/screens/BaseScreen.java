ee23bffff118b1ec62c3515da7dda69031ab9aed
placidmasvidal <contact@placidmasvidal.com>
2020-05-16 20:38:51 +0200

    some refactors done

--- core/src/com/xaviplacidpol/blindbloodblade/screens/BaseScreen.java
+++ core/src/com/xaviplacidpol/blindbloodblade/screens/BaseScreen.java
@@ -1,24 +1,16 @@
 package com.xaviplacidpol.blindbloodblade.screens;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.xaviplacidpol.blindbloodblade.BlindBloodBlade;
-import com.xaviplacidpol.blindbloodblade.utils.AssetManager;
 import com.xaviplacidpol.blindbloodblade.utils.Assets;
-
-import java.awt.Menu;
 
 public class BaseScreen implements Screen {
 
-//    private static final int screenWidth = Gdx.graphics.getWidth();
-//    private static final int screenHeight = Gdx.graphics.getHeight();
 
     protected final BlindBloodBlade game;
 
     public BaseScreen(final BlindBloodBlade game){
         this.game = game;
-
-   //     AssetManager.load();
 
         //Initialize the Assets instance
         com.badlogic.gdx.assets.AssetManager am = new com.badlogic.gdx.assets.AssetManager();
@@ -62,11 +54,4 @@ public class BaseScreen implements Screen {
 
     }
 
- /*   public static int getScreenWidth() {
-        return screenWidth;
-    }
-
-    public static int getScreenHeight() {
-        return screenHeight;
-    }   */
 }
